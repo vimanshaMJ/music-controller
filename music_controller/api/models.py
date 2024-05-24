e64528153fc8246ecf11840f730b190d42b7a9a7
vimansha <vimanshamadushani88@gmail.com>
2024-05-24 10:23:24 +0530

    five attributes for each room

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+   code = models.CharField(max_length=8, default="", unique=True)   # code-unique code that identifie a room
+   host = models.CharField(max_length=50, unique=True)  #one host for one room, one host cannot have multiple rooms
+   # host field - store some kinda information, essentially relates to our host
+
+   guest_can_pause = models.BooleanField(null=False, default=False)   #permission that says okay, is the guest allowed to pause the music or play the music
+   votes_to_skip = models.IntegerField(null=False, default=1)
+   create_at = models.DateTimeField(auto_now_add=True)  #auto_now_add- automatically add the date, time it was created at
+   
+
