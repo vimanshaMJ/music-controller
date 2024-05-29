7f97e51daa284d04ffff670bfdd4e84db4d90dc5
vimansha <vimanshamadushani88@gmail.com>
2024-05-29 23:05:42 +0530

    create the HomePage

---
+++ music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>This is the Home Page</p>
+      </div>
+    );
+  }
+}
