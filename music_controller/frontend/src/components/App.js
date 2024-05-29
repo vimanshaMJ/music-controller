a68c2b6a1d472f311034a7a3f1420cb38266fa9c
vimansha <vimanshamadushani88@gmail.com>
2024-05-29 23:10:01 +0530

    two more pages

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import HomePage from "./HomePage";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
 
 export default class App extends Component {
   constructor(props) {
@@ -8,7 +10,13 @@ export default class App extends Component {
   }
 
   render() {
-    return <HomePage />;
+    return (
+      <div>
+        <HomePage />
+        <RoomJoinPage />
+        <CreateRoomPage />
+      </div>
+    );
   }
 }
 
--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class RoomJoinPage extends Component {
+export default class CreateRoomPage extends Component {
   constructor(props) {
     super(props);
   }
@@ -8,7 +8,7 @@ export default class RoomJoinPage extends Component {
   render() {
     return (
       <div>
-        <p>This is the Room Join Page</p>
+        <p>This is the Create Room Page</p>
       </div>
     );
   }
