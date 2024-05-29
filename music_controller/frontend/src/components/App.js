240e475b6f2a4c0dea21461df63137169145875f
vimansha <vimanshamadushani88@gmail.com>
2024-05-29 22:36:38 +0530

    test using prop.name

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -7,9 +7,9 @@ export default class App extends Component {
   }
 
   render() {
-    return <h1>Testing react code</h1>;
+    return <h1>{this.props.name}</h1>;
   }
 }
 
 const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+render(<App name="Vimansha" />, appDiv);
