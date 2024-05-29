476e5a712918b84928c68ab278de01c233bd4b21
vimansha <vimanshamadushani88@gmail.com>
2024-05-29 23:47:50 +0530

    added paths

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
 
 export default class HomePage extends Component {
   constructor(props) {
@@ -7,9 +10,13 @@ export default class HomePage extends Component {
 
   render() {
     return (
-      <div>
-        <p>This is the Home Page</p>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/join" element={<RoomJoinPage />} />
+          <Route path="/create" element={<CreateRoomPage />} />
+          <Route path="/" element={<p>This is the homepage</p>} />
+        </Routes>
+      </Router>
     );
   }
 }
--- music_controller/frontend/urls.py
+++ music_controller/frontend/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import index
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('join', index),
+    path('create', index)
    
 ]
