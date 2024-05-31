9ca4c336cbe43b0fe09950023c2c653d4714c79d
vimansha <vimanshamadushani88@gmail.com>
2024-05-31 14:15:39 +0530

    class CreateRoomView and class Meta

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from .models import Room
-from .serializers import RoomSerializer
+from .serializers import RoomSerializer, CreateRoomSerializer
+from rest_framework.views import APIView
+from rest_framework.response import responses
 
 # Create your views here.
 
 class Roomview(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+    
+# APIView : allow to override some default methods 
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, format=None):
+        pass
     
